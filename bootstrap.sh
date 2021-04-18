142863a37b98e5b59ef5569047fed3d676c7e2d1
root <root@ip-172-31-17-62.ap-southeast-2.compute.internal>
2021-04-18 08:15:35 +0000

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+helm repo add argo-cd https://argoproj.github.io/argo-helm
+helm dep update charts/argo-cd/
+helm install argo-cd charts/argo-cd/
+helm template apps/ | kubectl apply -f -

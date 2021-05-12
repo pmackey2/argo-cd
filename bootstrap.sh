3dc99ecd928cbc36f1f45addd86e05350623d58f
root <root@rke2-cmd-server-1.chlg.sputnik.ves.is>
2021-05-12 22:09:28 +0000

    Move manifests into command and operations structure

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dep update charts/argo-cd/
 helm install argo-cd charts/argo-cd/
-helm template apps/ | kubectl apply -f -
+helm template apps/command/ | kubectl apply -f -
